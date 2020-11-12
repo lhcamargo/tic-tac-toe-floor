043f4e795baa5ae0d2a34a54370933dc49a8b5e9
lhcamargo <44513594+lhcamargo@users.noreply.github.com>
2020-11-12 20:28:44 -0300

    1.1
    tic tac floor

---
+++ lib/core/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const String PLAYER1_SYMBOL = 'Trump';
+const Color PLAYER1_COLOR = Colors.red; // Alfa RGB (0xFFFF0000)
+const String PLAYER1_IMG = 'assets/trump.png';
+
+const String PLAYER2_SYMBOL = 'Biden';
+const Color PLAYER2_COLOR = Colors.blue;
+const String PLAYER2_IMG = 'assets/biden.png';
+
+const String GAME_TITLE = 'Tic Tac Toe';
+const String TIED_TITLE = 'Game Tied!';
+const String WIN_TITLE = 'Player [SYMBOL] won!';
+const String DIALOG_MESSAGE = 'Press the reset button to start again!';
+const String SINGLE_PLAYER_MODE = 'Single Player';
+const String MULTIPLAYER_MODE = 'Two Players';
+const String RESET_BUTTON_LABEL = 'RESET';
